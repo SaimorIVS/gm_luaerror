d3085b9b879e7841d34f33570924310624fe777e
Mikkel Struntze <mikkel.struntze@gmail.com>
2023-04-12 21:59:30 +0200

    aa

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-PROJECT_GENERATOR_VERSION = 2
+PROJECT_GENERATOR_VERSION = 3
 
 newoption({
 	trigger = "gmcommon",
@@ -10,7 +10,7 @@ include(assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory"))
 
 CreateWorkspace({name = "luaerror", abi_compatible = true})
-	CreateProject({serverside = true, manual_files = true})
+	CreateProject({serverside = true})
 		IncludeLuaShared()
 		IncludeHelpersExtended()
 		IncludeSDKCommon()
@@ -27,34 +27,4 @@ CreateWorkspace({name = "luaerror", abi_compatible = true})
 			"source/shared/shared.hpp",
 			"source/common/common.cpp",
 			"source/common/common.hpp"
-		})
-
-	CreateProject({serverside = false, manual_files = true})
-		IncludeLuaShared()
-		IncludeHelpersExtended()
-		IncludeSDKCommon()
-		IncludeSDKTier0()
-		IncludeSDKTier1()
-		IncludeScanning()
-		IncludeDetouring()
-
-		files({
-			"source/shared/main.cpp",
-			"source/shared/shared.cpp",
-			"source/shared/shared.hpp",
-			"source/common/common.cpp",
-			"source/common/common.hpp"
-		})
-
-	project("testing")
-		kind("ConsoleApp")
-		includedirs("source/common")
-		files({
-			"source/common/common.hpp",
-			"source/common/common.cpp",
-			"source/testing/main.cpp"
-		})
-		vpaths({
-			["Header files/*"] = "source/**.hpp",
-			["Source files/*"] = "source/**.cpp"
 		})
